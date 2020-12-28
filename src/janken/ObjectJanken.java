7eb018924747f97df48d741ad33f83d290dbf862
1noseA <1noseA2L@gmail.com>
2020-12-28 14:15:48 +0900

    [ジャンケン]Lesson4オブジェクト指向で拡張

--- src/janken/ObjectJanken.java
+++ src/janken/ObjectJanken.java
@@ -5,11 +5,14 @@ public class ObjectJanken {
 		// 審判（斎藤さん）のインスタンス生成
 		Judge saito = new Judge();
 
+		// 左辺をPlayerとすることでJudgeクラスは
+		// MurataとYamadaをPlayerクラスとして扱うことができる
+
 		// プレイヤー1（村田さん）の生成
-		Player murata = new Player("村田さん");
+		Player murata = new Murata("村田さん");
 
 		// プレイヤー2（山田さん）の生成
-		Player yamada = new Player("山田さん");
+		Player yamada = new Yamada("山田さん");
 
 		// 村田さんと山田さんをプレイヤーとしてジャンケンを開始する
 		saito.startJanken(murata, yamada);
---
+++ src/janken/Yamada.java
@@ -0,0 +1,24 @@
+package janken;
+
+public class Yamada extends Player {
+	/**
+     * プレイヤークラスのコンストラクタ
+     *
+     * @param name 名前
+     */
+    public Yamada(String name) {
+    	// スーパークラスのコンストラクタを呼び出す
+    	super(name);
+    }
+
+    /**
+     * ジャンケンの手を出す
+     * スーパークラスの同名メソッドをオーバーライドしている
+     *
+     * @return ジャンケンの手
+     * */
+    public int showHand() {
+		// 必ずグーを出す
+		return PAPER;
+    }
+}
