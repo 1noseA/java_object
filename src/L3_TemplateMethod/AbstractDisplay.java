73b66f9d06a2f723e8cd420b1548e281ea3dad43
1noseA <1noseA2L@gmail.com>
2021-01-03 10:23:30 +0900

    [第3章]問題

--- src/L3_TemplateMethod/AbstractDisplay.java
+++ src/L3_TemplateMethod/AbstractDisplay.java
@@ -1,9 +1,14 @@
 package L3_TemplateMethod;
 
+// Template Methodパターンでは抽象クラスの代わりにインタフェースを使うことはできない
+// (抽象クラスでは一部のメソッドを具体的に実装できるが、インタフェースは抽象メソッドのみ)
 public abstract class AbstractDisplay {  // 抽象クラスAbstractDisplay
 	public abstract void open();         // サブクラスに実装を任せる抽象メソッド(1)open
 	public abstract void print();        // サブクラスに実装を任せる抽象メソッド(2)print
 	public abstract void close();        // サブクラスに実装を任せる抽象メソッド(3)close
+
+    // finalは「もしサブクラスを作るならdisplayメソッドをオーバーライドせずに機能拡張せよ」
+    // テンプレートメソッドはオーバーライドすべきでない
 	public final void display() {        // この抽象クラスで実装しているメソッドdisplay
 		open();                          // まずopenして…
 		for (int i = 0; i < 5; i++) {    // 5回printを繰り返して…
